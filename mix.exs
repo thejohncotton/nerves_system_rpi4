c84c8fca99c04ece69e192f4cce35454f9bbd8d0
John Cotton <john@1080people.com>
2024-09-28 16:55:57 -0400

    additional config for usb midi

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule NervesSystemRpi4.MixProject do
   use Mix.Project
 
   @github_organization "nerves-project"
-  @app :nerves_system_rpi4_alsa_midi_3
+  @app :nerves_system_rpi4_alsa_midi_4
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
